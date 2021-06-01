7beb3a714f4c13fc77dff9ce1a7c205c2f77f459
shivangigoel1302 <shivangigoel1301@gmail.com>
2021-06-01 17:53:54 +0530

    firebase initialized

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyC4uTnypmmzNR7dtJ4pWR7M_DVQI4wW0QE",
+    authDomain: "chat-web-app-d19ef.firebaseapp.com",
+    projectId: "chat-web-app-d19ef",
+    storageBucket: "chat-web-app-d19ef.appspot.com",
+    messagingSenderId: "1088602043433",
+    appId: "1:1088602043433:web:91090e2d6ae00fd413d838"
+  };
+
+  const app = firebase.initializeApp(config);
