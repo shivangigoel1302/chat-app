02e2873c63c1be99cd5705814039d54b5516ae30
shivangigoel1302 <shivangigoel1301@gmail.com>
2021-06-05 00:13:49 +0530

    dashboard components added

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Button, Divider, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import AvatarUploadBtn from './AvatarUploadBtn';
+import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({onSignOut}) =>{
     const {profile } = useProfile();
     const onSave = async newData => {
-        console.log(newData);
+        const userNicknameRef =database.ref(`/profiles/${profile.uid}`).child('name');
+        try{
+            await userNicknameRef.set(newData);
+            Alert.success('Nickname has been updated',4000);
+        }catch(err){
+            Alert.error(err.message,4000);
+        }
     };
     return(
         <>
@@ -17,6 +26,7 @@ const Dashboard = ({onSignOut}) =>{
           </Drawer.Header>
           <Drawer.Body>
               <h3>Hey, {profile.name}</h3>
+              <ProviderBlock/>
               <Divider/>
               <EditableInput
               name = "nickname"
@@ -24,6 +34,7 @@ const Dashboard = ({onSignOut}) =>{
               onSave = {onSave}
               label = {<h6 className="mb-2">Nickname</h6>}
               />
+              <AvatarUploadBtn/>
           </Drawer.Body>
           <Drawer.Footer>
             <Button block color = "red" onClick={onSignOut}>
